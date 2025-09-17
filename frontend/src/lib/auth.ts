dbed6a4c1e57729f7e179fa25f517922c8592026
Hejin Jo <hejinjo@hejins-air.mynetworksettings.com>
2025-09-16 22:01:01 -0400

    Dont log

--- frontend/src/lib/auth.ts
+++ frontend/src/lib/auth.ts
@@ -3,8 +3,6 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAPIKey = import.meta.env.VITE_SUPABASE_DEFAULT_KEY;
 
-console.log(supabaseUrl, supabaseAPIKey);
-
 if (!supabaseUrl || !supabaseAPIKey) {
   throw new Error('Missing Supabase environment variables');
 }
